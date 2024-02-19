005f4c9040ae363d50a621db6e1810bfa290ea3f
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-02-19 11:18:07 +0000

    Injection des dépendences avec version Annotation

--- src/main/java/dao/DaoImpl.java
+++ src/main/java/dao/DaoImpl.java
@@ -1,5 +1,8 @@
 package dao;
 
+import org.springframework.stereotype.Component;
+
+@Component("dao")
 public class DaoImpl implements IDao
 {
     @Override
--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -1,9 +1,13 @@
 package metier;
 
 import dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component("metier")
 public class MetierImpl implements IMetier{
     //couplage faible
+    @Autowired
     private IDao dao ;
     public MetierImpl() {
 
